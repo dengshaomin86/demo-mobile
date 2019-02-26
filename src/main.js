f17bc9d0f247c943423091df81cb8cbb89a2ad75
dsm <1148130879@qq.com>
2019-02-26 11:45:43 +0800

    1. 修正

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ import './assets/sass/common.scss'
 
 // slide-verify
 import slideVerify from '../static/slide-verify/slide-verify'
+Vue.use(slideVerify)
 
 // import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
-Vue.use(slideVerify)
-
+// 生产提示
 Vue.config.productionTip = false
 
 Vue.prototype.IsHasImg = (imgSrc, defaultSrc, cb) => {
