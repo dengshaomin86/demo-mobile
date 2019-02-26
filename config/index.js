e8411a4da43dbd47fe0a8ef1e7c58fe903b82770
dsm <1148130879@qq.com>
2019-02-26 10:12:56 +0800

    1. publicPath  打包静态文件路径配置

--- config/index.js
+++ config/index.js
@@ -48,7 +48,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/ROOT'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
