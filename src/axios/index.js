3eda7bb5f4472d8597417e97a88699b1b5c3637d
dsm <1148130879@qq.com>
2019-02-28 18:20:31 +0800

    1. 请求按钮

--- src/axios/index.js
+++ src/axios/index.js
@@ -12,18 +12,18 @@ axios.interceptors.request.use(function (config) {
     }
 
     // 按钮禁用
-    if (config.btn) {
-        console.log('btn disable', config.btn);
-        config.btn.setAttribute('disabled', true);
-        config.btn.setAttribute('data-text', config.btn.innerText);
-        config.btn.innerText = 'loading';
-        config.btn.style.opacity = '0.7';
+    let btn = config.btn;
+    if (btn) {
+        btn.setAttribute('disabled', true);
+        btn.setAttribute('data-text', btn.innerText);
+        btn.innerText = 'loading';
+        btn.style.opacity = '0.7';
     }
 
-    return config;
+    return config
 }, function (error) {
     // 对请求错误做些什么
-    return Promise.reject(error);
+    return Promise.reject(error)
 });
 
 // 添加响应拦截器
@@ -31,12 +31,21 @@ axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
     // console.log('loading end', response);
     Vue.prototype.$loading.close();
-    return response;
+
+    // 按钮解禁
+    let btn = response.config.btn;
+    if (btn) {
+        btn.removeAttribute('disabled');
+        btn.innerText = btn.getAttribute('data-text');
+        btn.style.opacity = '1';
+    }
+
+    return response
 }, function (error) {
     // 对响应错误做点什么
     console.log('error', 'loading end', error);
     Vue.prototype.$loading.close();
-    return Promise.reject(error);
+    return Promise.reject(error)
 });
 
 window.axios = axios;
