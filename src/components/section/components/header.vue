2baf052f69654106bada98faef42aa1aaf9c6f44
dsm <1148130879@qq.com>
2019-03-15 17:48:03 +0800

    1. svg

--- src/components/section/components/header.vue
+++ src/components/section/components/header.vue
@@ -4,7 +4,7 @@
     <header>
         <div class="back" @click="$back">
             <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" version="1.1">
-                <path d="M13 8 L2 20 L13 32 M2 20 L30 20" fill="none" stroke="#fff" stroke-width="3"></path>
+                <path d="M13,8 L2,20 L13,32 M2,20 L30,20" fill="none" stroke="#fff" stroke-width="3"></path>
             </svg>
         </div>
         <slot>
--- src/components/svg.vue
+++ src/components/svg.vue
@@ -18,7 +18,6 @@
                 </text>
             </svg>
 
-
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                 <defs>
                     <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
@@ -38,6 +37,20 @@
                 </defs>
                 <ellipse cx="200" cy="70" rx="85" ry="55" fill="url(#grad1)"></ellipse>
                 <text fill="#ffffff" font-size="45" font-family="Verdana" x="150" y="86">SVG</text>
+            </svg>
+
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <defs>
+                    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1"></stop>
+                        <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1"></stop>
+                    </linearGradient>
+                </defs>
+                <path d="M10,10 a4,3 0 0,0 30,0" fill="none" stroke="url(#grad1)" stroke-width="3"></path>
+            </svg>
+
+            <svg width="80" height="80" color="yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path fill="currentColor" d="M224 420c-11 0-20-9-20-20v-64c0-11 9-20 20-20s20 9 20 20v64c0 11-9 20-20 20zm224-148v192c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48h16v-64C64 71.6 136-.3 224.5 0 312.9.3 384 73.1 384 161.5V224h16c26.5 0 48 21.5 48 48zM96 224h256v-64c0-70.6-57.4-128-128-128S96 89.4 96 160v64zm320 240V272c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v192c0 8.8 7.2 16 16 16h352c8.8 0 16-7.2 16-16z" class=""></path>
             </svg>
         </main>
 
