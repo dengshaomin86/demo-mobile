4c3b10f225c85e0b662eb002ac4d459b1a385268
dsm <1148130879@qq.com>
2019-03-29 18:02:13 +0800

    1. grid

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -4,10 +4,44 @@
             <h1>grid</h1>
         </v-header>
 
+        <div class="con">
+            <div class="t"></div>
+            <div class="b">
+                <ul>
+                    <li>123456</li>
+                </ul>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <style scoped lang="scss">
+    .tl-con {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .con {
+        width: 100%;
+        height: 100%;
+        background-color: cadetblue;
+        overflow-x: hidden;
+        display: grid;
+        grid-gap: 0.4rem;
+
+        .t {
+            width: 100%;
+            min-height: 2rem;
+            background-color: blue;
+        }
+        .b {
+            width: 100%;
+            min-height: 2rem;
+            background-color: rebeccapurple;
+        }
+    }
 
 </style>
 
