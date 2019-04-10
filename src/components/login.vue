e0d29d4af02006b94e16b1ec8c350aea03a6a578
dsm <1148130879@qq.com>
2019-04-10 16:05:08 +0800

    1. 水波纹

--- src/components/login.vue
+++ src/components/login.vue
@@ -158,30 +158,27 @@
     .ripple {
         position: relative;
         overflow: hidden;
+        &:after {
+            display: block;
+            content: "";
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            pointer-events: none;
+            background: radial-gradient(circle, #666 10%, transparent 10%) no-repeat center;
+            transform: scale(10, 10);
+            opacity: 0;
+            transition: transform .3s, opacity .5s;
+        }
+        &:active:after {
+            transform: scale(0, 0);
+            opacity: .3;
+            transition: 0s;
+        }
     }
 
-    .ripple:after {
-        content: "";
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        pointer-events: none;
-        background-image: radial-gradient(circle, #666 10%, transparent 10%);
-        background-repeat: no-repeat;
-        background-position: 50%;
-        transform: scale(10, 10);
-        opacity: 0;
-        transition: transform .3s, opacity .5s;
-    }
-
-    .ripple:active:after {
-        transform: scale(0, 0);
-        opacity: .3;
-        transition: 0s;
-    }
 </style>
 
 <script>
