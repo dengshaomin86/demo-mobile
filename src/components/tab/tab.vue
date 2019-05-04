a9b1b6988297f70151a6e77664dc36add52ca285
dengshaomin86 <1101869752@qq.com>
2019-05-05 01:10:10 +0800

    1. theme

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
-        <keep-alive>
-            <router-view></router-view>
-        </keep-alive>
+    <div class="tl-con tab-con">
+        <div class="tab-main">
+            <keep-alive>
+                <router-view></router-view>
+            </keep-alive>
+        </div>
+
+        <ul class="tab">
+            <router-link tag="li" to="/index">首页</router-link>
+            <router-link tag="li" to="/mine">我的</router-link>
+        </ul>
     </div>
 </template>
 
@@ -13,8 +20,26 @@
 </script>
 
 <style scoped lang="scss">
-    div {
-        width: 100%;
-        height: 100%;
+    .tab-con {
+        display: flex;
+        flex-direction: column;
+        .tab-main {
+            width: 100%;
+            height: 100%;
+            overflow-x: hidden;
+        }
+        .tab {
+            flex: 0 0 1.5rem;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            background-color: $bgPanel;
+            border-top: 1px solid $border;
+            li {
+                &.router-link-active {
+                    color: $active;
+                }
+            }
+        }
     }
 </style>
